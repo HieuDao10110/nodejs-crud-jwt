cc775855f2b53edcccd7f0c74ef5b8bc0a52bf04
hieudao10110 <daovanhieu10110@gmail.com>
2023-01-03 03:16:55 +0700

    update package

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require("express");
 const db = require("./models");
 const bodyParser = require('body-parser')
 const simpleData = require('./node/simpleData')
+const logger = require('./config/logger')
 
 var indexRouter = require('./routes/index.js');
 var userRouter = require('./routes/userRoutes.js');
 var editorRouter = require('./routes/editorRoutes.js');
 var managerRouter = require('./routes/managerRoutes.js');
 var adminRouter = require('./routes/adminRoutes.js');
+const {transports} = require("winston");
 
 const app = express();
+
+// Handling Uncaught Exceptions with winston
+logger.exceptions.handle(
+    new transports.File({ filename: 'log/exceptions.log' })
+);
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
