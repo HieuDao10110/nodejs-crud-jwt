c1f52fcbd38a88b486b3dfef9a4d5df3d45ec73e
Ubuntu <ubuntu@ip-172-31-45-219.ap-southeast-1.compute.internal>
2023-02-24 01:42:08 +0000

    add free ssl with certbot

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const db = require("./models");
 const bodyParser = require('body-parser')
 const simpleData = require('./node/simpleData')
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 var indexRouter = require('./routes/index.js');
 var getRouter = require('./routes/getUserRoutes');
@@ -25,6 +28,17 @@ app.use('/crud', getRouter, listRouter, createRouter, updateRouter, deleteRouter
 // app.use('/admin', updateRouter);
 // app.use('/admin', deleteRouter);
 
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/self.gofa.vn/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/self.gofa.vn/fullchain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/self.gofa.vn/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 asyncDatabase = async (createData) => {
 
     await db.sequelize.sync()
@@ -44,10 +58,22 @@ asyncDatabase = async (createData) => {
 }
 asyncDatabase(false);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080, () => {
+	console.log('HTTP Server running on port 8080');
 });
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+// set port, listen for requests
+//const PORT = process.env.PORT || 8080;
+//app.listen(PORT, () => {
+//    console.log(`Server is running on port ${PORT}.`);
+//});
 
 module.exports = app;
